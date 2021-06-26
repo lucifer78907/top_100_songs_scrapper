d6557a85985551a86309b4a6be916ac694a9e7b0
Rudra <chaudhary66667@gmail.com>
2021-06-26 18:11:36 +0530

    Top 100 names scraped

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-user_input = input("Which year you want to travel to ?"
-                   "\nType the date in YYYY-MM-DD Format\n")
-print(user_input)
+from bs4 import BeautifulSoup
+import requests
+
+# user_input = input("Which year you want to travel to ?"
+#                    "\nType the date in YYYY-MM-DD Format\n")
+# print(user_input)
+
+
+# -------------------------------------SCRAPING USING Beautiful SOUP ---------------#
+response = requests.get("https://www.billboard.com/charts/hot-100/2000-08-12")
+web_data = response.text
+
+soup = BeautifulSoup(web_data,"html.parser")
+
+ele_list = soup.find_all(name="span",class_="chart-element__information__song text--truncate color--primary")
+names_list = [items.getText() for items in ele_list]
+print(names_list)
